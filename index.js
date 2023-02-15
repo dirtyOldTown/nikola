d400149eda301da9a2ee310f09895fb3f2444fe4
Aleksandar Pejinović <boloks70@gmail.com>
2023-02-15 19:33:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -78,10 +78,10 @@ function slajd() {
    slika1.style.opacity = 0;
    slika2.style.opacity = 0;
    setTimeout(() => {
-     slika1.src = slikeSrc[1];
-     slika2.src = slikeSrc2[1];
      slika1.style.opacity = 1;
      slika2.style.opacity = 1;
+     slika1.src = slikeSrc[1];
+     slika2.src = slikeSrc2[1];
    }, 1000);
 }
 let timer = setTimeout(function prikaziSlajd() {
