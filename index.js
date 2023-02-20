0f1b66c9debb0e3369754fc90b88bf5110839ff2
Aleksandar Pejinović <boloks70@gmail.com>
2023-02-20 19:27:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -76,15 +76,17 @@ function slajd() {
    slika1.style.opacity = 0;
    slika2.style.opacity = 0;
    setTimeout(() => {
-     slika1.style.opacity = 1;
-     slika2.style.opacity = 1;
      slika1.src = slikeSrc[1];
      slika2.src = slikeSrc2[1];
    }, 800);
    setTimeout(() => {
+      slika1.style.opacity = 1;
+      slika2.style.opacity = 1;
+   }, 800);
+   setTimeout(() => {
       slikeSrc.reverse();
       slikeSrc2.reverse();
-   }, 800);
+   }, 900);
 }
 setInterval(slajd, 7000)
 
