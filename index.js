9ee9a7726fa6e6faed4944e1dae0be5f109650da
Aleksandar Pejinović <boloks70@gmail.com>
2023-02-21 21:43:43 +0100

    update all

--- index.js
+++ index.js
@@ -74,12 +74,12 @@ let slikeSrc = ["slike/slika2.jpg", "slike/slika3.jpg"];
 let slikeSrc2 = ["slike/slika1.jpg", "slike/slika4.jpg"];
 let slika1 = document.querySelector(".glava img:first-of-type");
 let slika2 = document.querySelector(".glava img:last-of-type");
+slika1.style.transition = 'all 0.8s';
+slika2.style.transition = 'all 0.8s';
 function slajd() {
    slika1.style.opacity = 0;
    slika2.style.opacity = 0;
    setTimeout(() => {
-      slika1.style.transition = 'all 0.8s';
-      slika2.style.transition = 'all 0.8s';
      slika1.style.opacity = 1;
      slika2.style.opacity = 1;
      slika1.src = slikeSrc[1];
